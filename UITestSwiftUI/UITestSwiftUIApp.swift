a4dbbaf83540d0a4b7518da13ebbfc8e41ef8114
umitmutluu <umitt.mmutlu@gmail.com>
2022-09-17 16:55:32 +0300

    first Commit

---
+++ UITestSwiftUI/UITestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestSwiftUIApp.swift
+//  UITestSwiftUI
+//
+//  Created by Umut Mutlu on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct UITestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
